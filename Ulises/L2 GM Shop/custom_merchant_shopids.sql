16345e45c2e84991a7f0c04aeab27832d99300ca
ueusebi <ueusebi@live.com.ar>
2017-05-29 20:02:13 -0300

    Add files via upload

---
+++ Ulises/L2 GM Shop/custom_merchant_shopids.sql
@@ -0,0 +1,3 @@
+INSERT INTO `custom_merchant_shopids` VALUES (3660300, '36603');
+INSERT INTO `custom_merchant_shopids` VALUES (3660301, '36603');
+INSERT INTO `custom_merchant_shopids` VALUES (3660302, '36603');
